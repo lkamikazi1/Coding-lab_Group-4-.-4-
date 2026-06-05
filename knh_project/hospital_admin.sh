6ec949721de714f854c211760b98a625c8337ade
DerrickMugire <d.mugire1@alustudent.com>
2026-06-05 02:06:43 -0700

    Restore hospital_admin.sh content

---
+++ knh_project/hospital_admin.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+initialize_system() {
+	echo "Initializing KNH system environment..."
+    if [ ! -d "active_logs" ]; then	    
+       echo "Creating active_logs directory..."
+        mkdir active_logs
+    fi
+    if [ ! -d "archived_logs" ]; then
+        echo "Creating archived_logs directory..."
+        mkdir archived_logs
+    fi
+    if [ ! -d "reports" ]; then
+        echo "Creating reports directory..."
+        mkdir reports
+    fi
+}
+ echo "system directories are ready"
+
+
+initialize_system
